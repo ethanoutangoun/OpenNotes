fa35b435d689d72d4f40c316220810c383c550d9
ethanoutangoun <ethanoutangoun@gmail.com>
2025-03-17 19:06:13 -0700

    Update theme

--- app/src/main/java/com/opennotes/ui/theme/Theme.kt
+++ app/src/main/java/com/opennotes/ui/theme/Theme.kt
@@ -19,7 +19,7 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = PrimaryColor,
     secondary = PurpleGrey40,
     tertiary = Pink40,
     surfaceVariant = GrayBackground, // Using GrayBackground as a surface variant
